4b4b7371d3d77bbb78a28e9526017fe1a5667cc6
Aluno <Aluno@045LAB02003>
2017-11-09 19:57:58 -0200

    Criando a classe CharacterBase e o projeto

---
+++ RpgSaaS/EntityBase/CharacterBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityBase
+{
+    class CharacterBase
+    {
+
+        public int maxHp { get; set; }
+        public int maxMp { get; set; }
+        public float currentHp { get; set; }
+        public float currentMp { get; set; }
+        public int Level { get; set; }
+        public int currentExp { get; set; }
+        public int nextLevelExp { get; set; }
+        public float difficultFactor { get; set; }
+        public Attributes attributes { get; set; }
+        public ArrayList<Item> equipedItems { get; set; }
+        public Inventory inventory { get; set; }
+        public Race race { get; set; }
+
+    }
+}
